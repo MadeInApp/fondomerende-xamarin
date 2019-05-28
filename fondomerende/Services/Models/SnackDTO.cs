df583de64dd4cf3b745a11c4b255251c70317e0d
Giulio <giuliozaccagnini22@gmail.com>
2019-05-28 16:34:12 +0200

    snack

---
+++ fondomerende/Services/Models/SnackDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fondomerende.Services.Models
+{
+    class SnackDTO
+    {
+        public ResponseDTO response;
+        public List<SnackDataDTO> data;
+    }
+}
