8712091728bca5b7ca5cbcdfedaaaf56e8c2f76e
Stefano Vecchietti <stefano.vecchietti.99@gmail.com>
2019-05-28 14:56:19 +0200

    Trasportato Layout e codice da MainPage a LoginPage

--- fondomerende/App.xaml.cs
+++ fondomerende/App.xaml.cs
@@ -10,7 +10,7 @@ namespace fondomerende
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new LoginPage();
         }
 
         protected override void OnStart()
---
+++ fondomerende/LoginPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using fondomerende.Services.RESTServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace fondomerende
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class LoginPage : ContentPage
+    {
+        private string username, password;
+        public LoginPage()
+        {
+            InitializeComponent();
+            macchinetta_immagine.Source = ImageSource.FromResource("fondomerende.image.macchinetta_merende.png");
+        }
+
+        private async void Bottone_ClickedAsync(object sender, EventArgs e)
+        {
+
+            if (!string.IsNullOrEmpty(usernameEntry.Text) && !string.IsNullOrEmpty(passwordEntry.Text))
+            {
+                username = usernameEntry.Text;
+                password = passwordEntry.Text;
+                LoginServiceManager loginService = new LoginServiceManager();
+                var response = await loginService.LoginAsync(username, password);
+
+            }
+            else
+            {
+                await DisplayAlert("Fondo Merende", "Username o Password mancanti", "OK");
+            }
+        }
+    }
+}
+
