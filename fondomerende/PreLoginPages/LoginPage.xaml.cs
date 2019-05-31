eb56356ed38e123b2a033719e4a54ec5551faa6e
Stefano Vecchietti <stefano.vecchietti.99@gmail.com>
2019-05-31 11:30:10 +0200

    Cambio Log In Lato iOS

--- fondomerende/PreLoginPages/LoginPage.xaml.cs
+++ fondomerende/PreLoginPages/LoginPage.xaml.cs
@@ -25,6 +25,7 @@ namespace fondomerende
             InitializeComponent();
             Fondo_Merende_logo.Source = ImageSource.FromResource("fondomerende.image.macchinettaNew.png");
             CheckBox.Source = ImageSource.FromResource("fondomerende.image.CheckBox_empty_32x32.png");
+            CheckBox_iOS.Source = ImageSource.FromResource("fondomerende.image.CheckBox_empty_32x32.png");
             NavigationPage.SetHasNavigationBar(this, false);
         }
 
@@ -35,12 +36,14 @@ namespace fondomerende
                 clicked = false;
                 remember = !remember;
                 CheckBox.Source = ImageSource.FromResource("fondomerende.image.CheckBox_empty_32x32.png");
+                CheckBox_iOS.Source = ImageSource.FromResource("fondomerende.image.CheckBox_empty_32x32.png");
             }
             else
             {
                 clicked = true;
                 remember = !remember;
                 CheckBox.Source = ImageSource.FromResource("fondomerende.image.CheckBox_Checked_32x32.png");
+                CheckBox_iOS.Source = ImageSource.FromResource("fondomerende.image.CheckBox_Checked_32x32.png");
             }
 
         }
