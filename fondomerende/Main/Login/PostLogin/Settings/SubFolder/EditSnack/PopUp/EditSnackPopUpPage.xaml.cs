e210c69b5cffb30023653cf511cdedb7909d07ca
Stefano Vecchietti <stefano.vecchietti.99@gmail.com>
2019-06-13 15:41:48 +0200

    No More

--- fondomerende/Main/Login/PostLogin/Settings/SubFolder/EditSnack/PopUp/EditSnackPopUpPage.xaml.cs
+++ fondomerende/Main/Login/PostLogin/Settings/SubFolder/EditSnack/PopUp/EditSnackPopUpPage.xaml.cs
@@ -16,11 +16,12 @@ namespace fondomerende.Main.Login.PostLogin.Settings.SubFolder.EditSnack.PopUp
     public partial class EditSnackPopUpPage : Rg.Plugins.Popup.Pages.PopupPage
     {
         SnackServiceManager snackService = new SnackServiceManager();
+        int snackID = EditSnackListPage.SelectedSnackID;
+        bool Swapped = true;
         int Quantity;
         public EditSnackPopUpPage()
         {
             InitializeComponent();
-            idSnack.Placeholder = Convert.ToString(EditSnackListPage.SelectedSnackID);
             NomeSnack.Placeholder = EditSnackListPage.SelectedSnackName;
             PrezzoSnack.Placeholder = Convert.ToString(EditSnackListPage.SelectedSnackPrice);
             SnackPerBox.Placeholder = Convert.ToString(EditSnackListPage.SelectedSnackPerBox);
@@ -40,6 +41,7 @@ namespace fondomerende.Main.Login.PostLogin.Settings.SubFolder.EditSnack.PopUp
                     if(result.data.snacks[i].id == EditSnackListPage.SelectedSnackID)
                     {
                         Qta.Placeholder = Convert.ToString(result.data.snacks[i].quantity);
+                        Quantity = result.data.snacks[i].quantity;
                         return;
                     }
                 }
@@ -120,8 +122,11 @@ namespace fondomerende.Main.Login.PostLogin.Settings.SubFolder.EditSnack.PopUp
 
         private async void Apply_Clicked(object sender, EventArgs e)
         {
-          
-
+           var res = await snackService.EditSnackAsync(snackID, NomeSnack.Text, PrezzoSnack.Text, SnackPerBox.Text, ExpInDays.Text,Int32.Parse(Qta.Text));
+            if (res.response.success)
+            {
+                await Navigation.PopPopupAsync();
+            }
         }
 
         private async void Discard_Clicked(object sender, EventArgs e)
@@ -129,7 +134,69 @@ namespace fondomerende.Main.Login.PostLogin.Settings.SubFolder.EditSnack.PopUp
             await Navigation.PopPopupAsync();
         }
 
+        private void Swap_Clicked(object sender, EventArgs e)
+        {
 
+            if (Swapped == true)
+            {
+                Swap.BackgroundColor = Color.Orange;
+                idSnack.Placeholder = null;
+                NomeSnack.Placeholder = null;
+                PrezzoSnack.Placeholder = null;
+                SnackPerBox.Placeholder = null;
+                ExpInDays.Placeholder = null;
+                if(idSnack.Text == null)
+                {
+                    idSnack.Text = Convert.ToString(EditSnackListPage.SelectedSnackID);
+                }
+                if(NomeSnack.Text == null)
+                {
+                    NomeSnack.Text = EditSnackListPage.SelectedSnackName;
+                }
+                if(PrezzoSnack.Text == null)
+                {
+                    PrezzoSnack.Text = Convert.ToString(EditSnackListPage.SelectedSnackPrice);
+                }
+                if(SnackPerBox.Text == null)
+                {
+                    SnackPerBox.Text = Convert.ToString(EditSnackListPage.SelectedSnackPerBox);
+                }
+                if(ExpInDays.Text == null)
+                {
+                  ExpInDays.Text = Convert.ToString(EditSnackListPage.SelectedSnackExpiration);
+                }
+                if(Qta.Text == null)
+                {
+                    Qta.Text = Convert.ToString(Quantity); 
+                }
+                Swapped = false;
+            }
+            else
+            {
+                
+                Swap.BackgroundColor = Color.Transparent;
+                idSnack.Text = null;
+                NomeSnack.Text = null;
+                PrezzoSnack.Text = null;
+                SnackPerBox.Text = null;
+                ExpInDays.Text = null;
+                idSnack.Placeholder = Convert.ToString(EditSnackListPage.SelectedSnackID);
+                NomeSnack.Placeholder = EditSnackListPage.SelectedSnackName;
+                PrezzoSnack.Placeholder = Convert.ToString(EditSnackListPage.SelectedSnackPrice);
+                SnackPerBox.Placeholder = Convert.ToString(EditSnackListPage.SelectedSnackPerBox);
+                ExpInDays.Placeholder = Convert.ToString(EditSnackListPage.SelectedSnackExpiration);
+                Swapped = true;
+            }
+        }
 
+        private void Discard_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Apply_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
