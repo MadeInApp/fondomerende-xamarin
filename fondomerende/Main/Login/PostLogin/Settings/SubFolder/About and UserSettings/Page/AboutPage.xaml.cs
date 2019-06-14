eb1429b2b701f49bfa5c7469622b515f4ad0d1a4
Stefano Vecchietti <stefano.vecchietti.99@gmail.com>
2019-06-14 10:43:31 +0200

    About

---
+++ fondomerende/Main/Login/PostLogin/Settings/SubFolder/About and UserSettings/Page/AboutPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace fondomerende.Main.Login.PostLogin.Settings.SubFolder.About_and_UserSettings.Page
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : TabbedPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
