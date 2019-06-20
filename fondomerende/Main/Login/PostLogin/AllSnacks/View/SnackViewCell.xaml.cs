44ccdeddade2da4d82a4d8ce20c97259273f771b
Stefano Vecchietti <stefano.vecchietti.99@gmail.com>
2019-06-20 10:11:12 +0200

    pacman imp

--- fondomerende/Main/Login/PostLogin/AllSnacks/View/SnackViewCell.xaml.cs
+++ fondomerende/Main/Login/PostLogin/AllSnacks/View/SnackViewCell.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lottie.Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,11 +13,35 @@ namespace fondomerende.Main.Login.PostLogin.AllSnacks.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SnackViewCell : ViewCell
     {
+        AnimationView pacMananimation;
         public SnackViewCell()
         {
             InitializeComponent();
-           
 
+
+            pacMananimation = new AnimationView
+            {
+                Animation = "pacman0.6.json",
+                Loop = true,
+                HorizontalOptions = LayoutOptions.StartAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                AutoPlay = true,
+                InputTransparent = true,
+                IsVisible = true,
+                Margin = new Thickness(0,0,0,0)
+            };
+
+            PacManTraslate();
+
+        }
+
+        async void PacManTraslate()
+        {
+           Stack.Children.Add(pacMananimation);
+           await Task.WhenAny<bool>
+            (
+             pacMananimation.TranslateTo(150,0, 10000)
+            );
         }
     }
 }
