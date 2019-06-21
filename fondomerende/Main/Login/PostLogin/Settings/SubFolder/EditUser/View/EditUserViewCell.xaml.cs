2eb1cec8255c87987ef90f3b78231f3a9d5d0155
daniel <gate8025@gmail.com>
2019-06-21 16:05:02 +0200

    another fix

--- fondomerende/Main/Login/PostLogin/Settings/SubFolder/EditUser/View/EditUserViewCell.xaml.cs
+++ fondomerende/Main/Login/PostLogin/Settings/SubFolder/EditUser/View/EditUserViewCell.xaml.cs
@@ -30,6 +30,14 @@ namespace fondomerende.Main.Login.PostLogin.Settings.SubFolder.EditUser.View
             First_letter();
             GetUserFundsMethod();
             friendly_name.Text = InformationFriendlyName();
+            //if(friendly_name.Text.Length >9)
+            //{
+                string appoggio = friendly_name.Text;
+                friendly_name.Text = "";
+                friendly_name.Text += appoggio.Substring(0,9);
+                friendly_name.Text += "...";
+            //}
+
             Cerchio.BackgroundColor = Color.FromHex(Preferences.Get("Colore", "#CCCCCC"));
             CerchioRc.FillColor = Color.FromHex(Preferences.Get("Colore", "#CCCCCC"));
             CerchioRc.BackgroundColor = Color.FromHex(Preferences.Get("Colore", "#CCCCCC"));
