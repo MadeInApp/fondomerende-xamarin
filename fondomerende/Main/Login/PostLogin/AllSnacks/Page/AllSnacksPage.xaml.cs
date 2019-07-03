6f5ef40e78ca2e8b9172becfb0acbbd663bbfc3d
Daniel <stefano.vecchietti.99@gmail.com>
2019-07-03 12:00:34 +0200

    best aereoplano

--- fondomerende/Main/Login/PostLogin/AllSnacks/Page/AllSnacksPage.xaml.cs
+++ fondomerende/Main/Login/PostLogin/AllSnacks/Page/AllSnacksPage.xaml.cs
@@ -79,9 +79,9 @@ namespace fondomerende.Main.Login.PostLogin.AllSnack.Page
             {              //                                                                 || \\                                    
                                                                    //                         ||  \\ Se il dispositivo è Android non mostra la Top Bar della Navigation Page,
                 case Device.Android: //                                             \\        ||   \\   Se è iOS invece si (perchè senza è una schifezza)
-                    NavigationPage.SetHasNavigationBar(this, false);//                \\      ||    \\         \                
-                    break;                                                               //////////////////////|
-                                                                                      //      ||    //         /       
+                    NavigationPage.SetHasNavigationBar(this, false);//                \\      ||    \\        \                
+                    break;     //                                                      ||||||||||||||||\/\/|    |
+                                                                                      //      ||    //        /       
                 default:                                                            //        ||   //
                     NavigationPage.SetHasNavigationBar(this, true);//                         ||  //
                     break;  //                                                                || //
